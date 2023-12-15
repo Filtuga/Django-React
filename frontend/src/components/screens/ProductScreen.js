93a58f03a77f64b8c5aa3d5f04eca9fa5b16b4ff
Filipe <filipemarquesferrao@gmail.com>
2023-12-15 14:54:48 +0000

    Removed products.js from FE, since they are fetched from BE

--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -1,78 +1,98 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import LinkContainer from 'react-router-bootstrap'
-import {Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
-import products from '../../products'
-import { useNavigate, useParams } from 'react-router-dom';
-import Rating from '../Rating'
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import Rating from '../Rating';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function ProductScreen() {
-    const {id} = useParams();
-    const product = products.find((p)=>p.id === id);
+  const [fetchedProducts, setFetchedProducts] = useState([]);
+  const { id } = useParams();
 
-    if (!product) {
-        return (
-            <h1 className='mt-5'>Product not found...</h1>
-        )
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const { data } = await axios.get('http://localhost:8000/api/products/');
+        setFetchedProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     }
+    fetchProducts();
+  }, []);
 
-    return (
-        <div>
-            <Link to='/' className='btn btn-dark my-3'> Go back </Link>
+  // Find the product with the matching id
+  const product = fetchedProducts.find((p) => p.id === id);
 
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name}></Image>
-                </Col>
+  if (!product) {
+    return <h1 className='mt-5'>Product not found...</h1>;
+  }
 
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={product.rating} color={"#f8e825"}></Rating>
-                            <div>{product.numReviews} reviews</div>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <h3>{product.price} €</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <h3>{product.description}</h3>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
+  return (
+    <div>
+      <Link to='/' className='btn btn-dark my-3'>
+        {' '}
+        Go back{' '}
+      </Link>
 
-                <Col>
-                <Card>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price:</Col>
-                                <Col>
-                                <strong>{product.price} €</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status:</Col>
-                                <Col>
-                                {product.countInStock > 0 ? "In stock" : "Out of stock"}
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Button variant='success' className='btn btn-block' disabled={product.countInStock === 0} type='button' >Add to Cart</Button>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
-                </Col>
-                
-            </Row>
-        </div>
-    )
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name}></Image>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} color={'#f8e825'}></Rating>
+              <div>{product.numReviews} reviews</div>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>{product.price} €</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>{product.description}</h3>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product.price} €</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  variant='success'
+                  className='btn btn-block'
+                  disabled={product.countInStock === 0}
+                  type='button'
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
