446a818e4e41c20c4bf7079be9b9fc7baf99a6ce
Filipe <filipemarquesferrao@gmail.com>
2023-12-19 10:35:33 +0000

    Created models and serialize Product model

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
+# from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializer import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -11,7 +13,9 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
